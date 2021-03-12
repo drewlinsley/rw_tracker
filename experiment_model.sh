48a464a5fc50b1e02a561d0d3d13f958d7d2b626
Drew Linsley <drewlinsley@gmail.com>
2021-03-12 12:23:59 -0500

    Fixed inference for circuit kys.

--- experiment_model.sh
+++ experiment_model.sh
@@ -1,3 +1,5 @@
 ## Update kys/default with checkpoint location. Copy checkpoints to pytracking/networks
-# python pytracking/run_tracker.py kys default  --dataset_name=got10k_val
-python pytracking/run_experiment.py myexperiments got_retrained_bl
+# python pytracking/run_experiment.py myexperiments got_retrained_bl  # BASELINE
+python pytracking/run_experiment.py myexperiments got_circuit_bl
+
+
--- pytracking/experiments/myexperiments.py
+++ pytracking/experiments/myexperiments.py
@@ -8,21 +8,9 @@ def got_retrained_bl():
     return trackers, dataset
 
 
-"""
-def atom_nfs_uav():
-    # Run three runs of ATOM on NFS and UAV datasets
-    trackers = trackerlist('atom', 'default', range(3))
-
-    dataset = get_dataset('nfs', 'uav')
+def got_circuit_bl():
+    # Evaluate GOT on a circuit KYS I trained
+    trackers = trackerlist('kysc', 'default', range(1))
+    dataset = get_dataset('got10k_test', 'got10k_val')  # , 'got10k_ltrval')
     return trackers, dataset
 
-
-def uav_test():
-    # Run DiMP18, ATOM and ECO on the UAV dataset
-    trackers = trackerlist('dimp', 'dimp18', range(1)) + \
-               trackerlist('atom', 'default', range(1)) + \
-               trackerlist('eco', 'default', range(1))
-
-    dataset = get_dataset('uav')
-    return trackers, dataset
-"""
